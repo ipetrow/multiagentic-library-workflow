f23f32e918560ecf054bb89fba667dd00807ea5e
ivan <ivan.s.petrov37@gmail.com>
2026-07-29 21:06:15 +0200

    change commits order

--- src/app/agents/library_agent.py
+++ src/app/agents/library_agent.py
@@ -1,16 +1,16 @@
-from src.app.adapters.mcp.models.models import ToolCallResponse
-from src.app.tools.definitions.tool_definitions import RETRIEVE_RECEIPT_BOOKS_TOOL
-from src.app.tools.tool_registry import ToolRegistry
-from src.app.domain.prompt.models import Prompt, PromptType
 from src.app.adapters.llm.base_service import LLMService
+from src.app.adapters.llm.models.llm_response import LLMResponse
+from src.app.adapters.mcp.models.models import ToolCallResponse
 from src.app.adapters.llm.models.models import (
     ContextRoleItem, 
     TextContent, 
     ContextToolOutputItem
 )
-from src.app.adapters.llm.models.llm_response import LLMResponse
+from src.app.domain.prompt.models import Prompt, PromptType
 from src.app.prompts.utils.prompts import load_prompt
 from src.app.skills.skill_registry import SkillRegistry
+from src.app.tools.definitions.tool_definitions import RETRIEVE_RECEIPT_BOOKS_TOOL
+from src.app.tools.tool_registry import ToolRegistry
 
 from .exceptions import MaxStepsExceededError
 
